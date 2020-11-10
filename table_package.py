108522873dfba39c467514aa94bff5e7a757c2d8
Matvey <bitcoineazy@gmail.com>
2020-11-10 16:28:38 +0300

    get_rows_by_number done

--- table_package.py
+++ table_package.py
@@ -1,38 +1,28 @@
 import csv
 import pickle
 import pandas
-import numpy
 
 class Package: #основной класс для csv/pickle load/save
     def __init__(self, file):#инициализация файлика(можно сделать и input'ом)
         self.file = file
 
     def load_table_csv(self): #загрузка во внутреннее представление модуля
-        directory = self.file
+        output_directory = r'C:\Users\79268\Dev\csvs\output.csv'
+        self.save_table_csv()
+        print(self.columns)
+        new_frame = pandas.DataFrame(self.frame)
+        new_frame.to_csv(output_directory)
+
 
     def save_table_csv(self): #сохранение из внутреннего представления модуля
         directory = self.file
-        election = pandas.read_csv(directory)
+        self.frame = pandas.read_csv(directory)
         # print(election)
-        election_modified = election.set_index('state')
-        print(election_modified)
+        frame_modified = self.frame.set_index('state')
+        print(self.frame)
+        self.columns = self.frame.columns.tolist()
 
 
-        '''
-        directory = self.file
-        representation_dictionary = {} #наше представление(ключи - заголовки, значения - списки)
-        with open(directory, newline='') as File:
-            reader = csv.reader(File)
-            csv_headings = next(reader)
-            print(len(csv_headings))
-            csv_values = []
-            for row in reader:
-                col = list(row[i] for i in range(len(csv_headings)))
-                csv_values.append(col)
-            for keys in csv_headings:
-                representation_dictionary.update({keys:csv_values})
-            print(representation_dictionary)
-        '''
     def load_table_pickle(self):
         directory = self.file
 
@@ -45,20 +35,28 @@ class Package: #основной класс для csv/pickle load/save
 
 class Operations(Package): # Модули с базовыми операциями над таблицами:
     # тут функции будут уже из родительского класса брать load,save и че-то делать...
-    def __init__(self, file, column, types):
+    def __init__(self, file):
         super().__init__(file) # унаследовали файлик из родительского класса
-        self.column = column # проинициализировали новые свойства column и types
-        self.types = types
+
         # Тут в перспективне будут инициализоровать подаваемые аргументы в функции ниже#
         # ниже во всех функциях доработаю подаваемые аргументы(types, column, values, copy_table, by_number и тд
         # также будем в функции будем подавать функции из главного класса - self.load_table_csv() и тд.
-    def get_rows_by_number(self):
+    def get_rows_by_number(self, start, stop, copy_table):
+        output_directory = r'C:\Users\79268\Dev\csvs\output.csv'
+        directory = self.file
+        if copy_table == True:
+            frame = pandas.read_csv(directory)
+            print(frame[start:stop+1])
+        elif copy_table == False:
+            frame = pandas.read_csv(directory)
+            frame[start:stop + 1].to_csv(output_directory, index=False)
+
         '''
         get_rows_by_number(start, [stop], copy_table=False) – получение таблицы из одной строки или из строк из интервала по номеру строки.
         Функция либо копирует исходные данные, либо создает новое представление таблицы, работающее с исходным набором данных (copy_table=False),
         таким образом изменения, внесенные через это представления будут наблюдаться и в исходной таблице.
         '''
-        pass
+
 
     def get_rows_by_index(self):
         '''
@@ -127,5 +125,8 @@ if __name__ == "__main__":
     #our_file = Package(r'C:\Users\79268\table_package\123123.csv.txt') #даём главному классу на вход наш файлик
     #our_file.load_table_csv() #наш файлик пройдёт через функцию и че-то сделает в перспективе(проверка главного класса)
     #functions = Operations(r'C:\Users\79268\table_package\123123.csv.txt', column= , types= ) #тут уже работаем с нашими настройками и функциями, как в задании и делаем что-то соответственно
-    ourfile = Package(r'C:\Users\79268\Dev\csvs\governors_county.csv')
-    ourfile.save_table_csv()
+    #ourfile = Package(r'C:\Users\79268\Dev\csvs\governors_county.csv')
+    #ourfile.save_table_csv()
+    #ourfile.load_table_csv()
+    operations = Operations(r'C:\Users\79268\Dev\csvs\governors_county.csv')
+    operations.get_rows_by_number(0,10,copy_table=False)
